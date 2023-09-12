efa6164c3fbaa0701e1ebadf9297b0f85199d86a
maladaska <github@kaoullas.com>
2023-09-12 15:11:17 +0100

    Cleanup - keep this project for generic dev tools setup

--- config/fish/config.fish
+++
@@ -1,7 +0,0 @@
-alias python='/usr/local/bin/python3'
-alias pip='/usr/local/bin/pip3'
-alias projects='cd ~/Projects'
-echo "Fish aliases set"
-
-# The next line updates PATH for the Google Cloud SDK.
-if [ -f '/Users/adamk/Applications/google-cloud-sdk/path.fish.inc' ]; . '/Users/adamk/Applications/google-cloud-sdk/path.fish.inc'; end
